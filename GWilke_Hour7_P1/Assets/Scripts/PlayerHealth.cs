08da5dadb4fd76a00875c2025db3a0c0274a8986
GabeWilke <gabewilke@gmail.com>
2024-09-30 03:41:27 +0200

    H7
    H7

---
+++ GWilke_Hour7_P1/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float health;
+            float poisonDamage;
+        health = 1004f;
+            poisonDamage = 125.5f;
+            Debug.Log(health);
+        health -= poisonDamage;
+        Debug.Log(health);
+        health -= poisonDamage;
+        Debug.Log(health);
+        health -= poisonDamage;
+        Debug.Log(health);
+        health -= poisonDamage;
+        Debug.Log(health);
+        health -= poisonDamage;
+        Debug.Log(health);
+        health -= poisonDamage;
+        Debug.Log(health);
+        health -= poisonDamage;
+        Debug.Log(health);
+        health -= poisonDamage;
+        Debug.Log(health);
+        Debug.Log("You no longer to exist");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
